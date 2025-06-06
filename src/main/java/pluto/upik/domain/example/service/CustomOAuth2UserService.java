2c6b8bd6e533c31842f7a4d76e3285f41824b0cd
eunbb <fakesilverpark@gmail.com>
2025-06-06 21:53:07 +0900

    feat: OAuth2 로그인 및 Security 기본 설정 추가

---
+++ src/main/java/pluto/upik/domain/example/service/CustomOAuth2UserService.java
@@ -0,0 +1,9 @@
+package pluto.upik.domain.example.service;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+}
---
+++ src/main/java/pluto/upik/shared/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package pluto.upik.shared.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import pluto.upik.domain.example.service.CustomOAuth2UserService;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuth2UserService)
+                        )
+                );
+
+        return http.build();
+    }
+}
