541e3839ec046b7cd9e2b394c18a0ca59472e1f2
eunbb <fakesilverpark@gmail.com>
2025-06-06 22:22:44 +0900

    feat: CustomOAuth2UserService 구현 완료

--- src/main/java/pluto/upik/shared/security/CustomOAuth2UserService.java
+++ src/main/java/pluto/upik/shared/security/CustomOAuth2UserService.java
@@ -1,9 +1,16 @@
 package pluto.upik.shared.security;
 
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest oAuth2UserRequest){
+        OAuth2User oAuth2User = super.loadUser(oAuth2UserRequest);
+        return new CustomUser(oAuth2User.getAttributes());
+    }
 }
