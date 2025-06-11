b0d4f8d681126dc7489d4da6e705537be6adab1f
eunbb <fakesilverpark@gmail.com>
2025-06-12 07:59:38 +0900

    chore: SecurityConfig에 JwtAuthenticationFilter 등록

--- src/main/java/pluto/upik/shared/config/SecurityConfig.java
+++ src/main/java/pluto/upik/shared/config/SecurityConfig.java
@@ -3,8 +3,15 @@ package pluto.upik.shared.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import pluto.upik.shared.jwt.JwtAuthenticationFilter;
+import pluto.upik.shared.jwt.JwtProvider;
 import pluto.upik.shared.security.KakaoOAuth2UserService;
 
 @Configuration
@@ -12,10 +19,14 @@ import pluto.upik.shared.security.KakaoOAuth2UserService;
 public class SecurityConfig {
 
     private final KakaoOAuth2UserService kakaoOAuth2UserService;
+    private final JwtProvider jwtProvider;
+
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().authenticated()
                 )
@@ -23,8 +34,15 @@ public class SecurityConfig {
                         .userInfoEndpoint(userInfo -> userInfo
                                 .userService(kakaoOAuth2UserService)
                         )
-                );
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+            throws Exception {
+        return config.getAuthenticationManager();
     }
 }
