5c48f9af5261785d26835aab819dae1a16130fab
eunbb <fakesilverpark@gmail.com>
2025-06-16 17:41:29 +0900

    fix: setter 를 @Data 어노테이션으로 대체

--- src/main/java/pluto/upik/shared/jwt/KakaoJwtProperties.java
+++ src/main/java/pluto/upik/shared/jwt/KakaoJwtProperties.java
@@ -1,21 +1,13 @@
 package pluto.upik.shared.jwt;
 
-import lombok.Getter;
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@Getter
+@Data
 @Component
 @ConfigurationProperties(prefix = "kakao.jwt")
 public class KakaoJwtProperties {
     private String secret;
     private long accessTokenExpirationMs;
-
-    public void setSecret(String secret) {
-        this.secret = secret;
-    }
-
-    public void setAccessTokenExpirationMs(long accessTokenExpirationMs) {
-        this.accessTokenExpirationMs = accessTokenExpirationMs;
-    }
 }
