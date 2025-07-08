c2e1b6c2ef240da24e08f85d04e8208fb6c8d254
eunbb <fakesilverpark@gmail.com>
2025-07-08 20:03:30 +0900

    경로별 접근 권한 설정 수정

--- src/main/java/pluto/upik/shared/config/SecurityConfig.java
+++ src/main/java/pluto/upik/shared/config/SecurityConfig.java
@@ -82,14 +82,14 @@ public class SecurityConfig {
                 .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig.userService(customOAuth2UserService))
                 .successHandler(customSuccessHandler));
 
-        // ★★★ 5. 경로별 접근 권한 설정
+        // 5. 경로별 접근 권한 설정
         http.authorizeHttpRequests(auth -> auth
                 // 프론트엔드 앱 로딩 및 인증 관련 경로는 누구나 접근 가능
-                .requestMatchers("/", "/oauth2/**", "/login/**", "/auth/reissue", "/auth/logout").permitAll()
+                .requestMatchers( "/oauth2/**", "/login/**", "/auth/reissue", "/auth/logout").permitAll()
                 // '/api/'로 시작하는 모든 요청은 반드시 인증이 필요함
                 .requestMatchers("/api/**").authenticated()
                 // 그 외 나머지 요청은 일단 허용 (프론트엔드 라우팅을 위해)
-                .anyRequest().permitAll()
+                .anyRequest().authenticated()
         );
 
         // 6. JWT 필터 추가
